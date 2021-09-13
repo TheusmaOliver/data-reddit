1e439b77c36ce96eb0a2197446949f206b354524
Matheus Rodrigues <matheusrodriguesoliveira273@gmail.com>
2021-09-13 13:45:45 -0300

    fix topbar and create more tests

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,28 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { firebase } from "../services/firebase";
 import { useHistory } from "react-router";
 import winninLogo from "../assets/images/winnin-logo.png";
+import { Menu } from "@material-ui/icons";
 import "../styles/topbar.css";
 export default function Topbar({ user }) {
   const router = useHistory();
-  console.log(user);
-  function loadUser() {
-    if (!user) {
-      return (
-        <button
-          onClick={() => router.push("/")}
-          className="header-info__button"
-        >
-          Login
-        </button>
-      );
-    }
-    return (
-      <img
-        className="header-image__avatar"
-        src={user?.avatar}
-        alt="foto de perfil"
-      />
-    );
+  const [menu, setMenu] = useState(false);
+
+  function logOut() {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        window.location.reload();
+      });
+  }
+
+  function mostrarMenu() {
+    setMenu(!menu);
   }
   return (
     <header className="header">
@@ -31,7 +27,42 @@ export default function Topbar({ user }) {
         <h1 className="header-title">
           Info-<span>Reddit</span>
         </h1>
-        <div className="header-info">{loadUser()}</div>
+
+        <div>
+          <p className="header-icone" onClick={() => mostrarMenu()}>
+            <Menu />
+          </p>
+          <ul className={`header-options ${menu ? "active" : ""}`}>
+            <li>
+              {!user ? (
+                <button
+                  onClick={() => router.push("/")}
+                  className="header-info__button"
+                >
+                  Login
+                </button>
+              ) : (
+                <>
+                  <img
+                    className="header-info__avatar"
+                    src={user?.avatar}
+                    alt="foto de perfil"
+                  />
+                  <span className="header-info__name">{user?.name}</span>
+                </>
+              )}
+            </li>
+            <li>
+              {!user ? (
+                ""
+              ) : (
+                <button className="header-info__button" onClick={logOut}>
+                  Log out
+                </button>
+              )}
+            </li>
+          </ul>
+        </div>
       </div>
     </header>
   );
--- src/pages/Login/Login.test.js
+++ src/pages/Login/Login.test.js
@@ -2,8 +2,15 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Login from "./Login";
 
-test('deve iniciar o texto do botão como "Login with google"', () => {
-  render(<Login />);
-  const btnLogin = screen.getByText("Login with google");
-  expect(btnLogin).toBeInTheDocument();
+describe("Login Component", () => {
+  test('deve iniciar o texto do botão como "Login with google"', () => {
+    render(<Login />);
+    const btnLogin = screen.getByText("Login with google");
+    expect(btnLogin).toBeInTheDocument();
+  });
+  test("deve conter a classe login-section__button no botão", () => {
+    render(<Login />);
+    const btnLogin = screen.getByText("Login with google");
+    expect(btnLogin).toHaveClass("login-section__button");
+  });
 });
